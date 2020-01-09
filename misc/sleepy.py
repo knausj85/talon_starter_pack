e54daa49cc6e7a870a29b10dfd15dad608a26d53
knausj85 <knaus.jeff@gmail.com>
2020-01-08 21:58:04 -0800

    add command "welcome back" to reenable eye tracking and "sleep all" to disable eye tracking and dragon
    - rename speech_toggle to sleepy, just because it makes more sense to me

--- misc/sleepy.py
+++ misc/sleepy.py
@@ -1,11 +1,20 @@
 from talon.voice import Context, ContextGroup
 from talon.engine import engine
-from talon_plugins import speech
+from talon_plugins import speech, eye_mouse, eye_zoom_mouse
 
 sleep_group = ContextGroup("sleepy")
 sleepy = Context("sleepy", group=sleep_group)
 
-sleepy.keymap(
+def wake_or_sleep(wake_up):
+    if wake_up:
+        eye_zoom_mouse.zoom_mouse.enable()
+        eye_mouse.control_mouse.enable() 
+    else:
+        eye_zoom_mouse.zoom_mouse.disable()
+        eye_mouse.control_mouse.disable()
+        engine.mimic("go to sleep".split())
+
+sleepy.keymap( 
     {
         "talon sleep": lambda m: speech.set_enabled(False),
         "talon wake": lambda m: speech.set_enabled(True),
@@ -17,6 +26,9 @@ sleepy.keymap(
             lambda m: speech.set_enabled(True),
             lambda m: engine.mimic("go to sleep".split()),
         ],
+        "sleep all" : lambda m: wake_or_sleep(False),
+        "welcome back": lambda m: wake_or_sleep(True),
     }
 )
+
 sleep_group.load()
